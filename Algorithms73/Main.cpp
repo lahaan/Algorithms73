6c2831730f3cd75c5626367edc64259aaf992f47
egg <ekemartinpaas@gmail.com>
2024-10-16 16:57:45 +0300

    Add project files.

---
+++ Algorithms73/Main.cpp
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "DateTime.h"
+#include "Objects.h"
+#include "Headers.h"
+#include "Structs.h"
+
+#pragma warning (disable : 4996)
+
+int main(){
+	HeaderD* pStruct = GetStruct7(3, 25);
+	return 0;
+}
