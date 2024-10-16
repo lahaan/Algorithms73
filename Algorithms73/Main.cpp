29627d502c8693b9e644767e8de6d81380460985
egg <ekemartinpaas@gmail.com>
2024-10-16 17:59:58 +0300

    printobj added

--- Algorithms73/Main.cpp
+++ Algorithms73/Main.cpp
@@ -6,7 +6,33 @@
 
 #pragma warning (disable : 4996)
 
-int main(){
-	HeaderD* pStruct = GetStruct7(3, 25);
+#define O 3
+#define N 25
+
+void PrintObjects(HeaderD* pStruct7) {
+	HeaderD* pTempStruct = pStruct7;
+	int i = 1, j = 1;
+
+	while (pTempStruct != NULL) {
+		// formatting string for printf is "%d) %s %lu %02d:%02d:%02d\n", the result is for example "1) Abcde 100 15:52:07"
+		printf("´[nr: %d] %c\n", i,pTempStruct->cBegin);
+		Object3* pTempObject = (Object3*)pTempStruct->pObject;
+		while (pTempObject != NULL) {
+			printf("\t (object %d): %s", j, pTempObject->pID);
+			printf("%lu %02d:%02d:%02d", pTempObject->Code, pTempObject->sTime1.Hour, pTempObject->sTime1.Minute, pTempObject->sTime1.Second);
+			pTempObject = pTempObject->pNext;
+		}
+		i++;
+	}
+
+
+
+	return;
+}
+
+
+int main() {
+	HeaderD* pStruct7 = GetStruct7(O, N);
+	PrintObjects(pStruct7);
 	return 0;
 }
